121dea0fb3aaab25b29581bbe6aad19a38dbbb82
Fabrizio <Fabrizio_39@hotmail.com>
2019-05-30 11:34:17 +0200

    add app.js file

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 4000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}...`);
+});
+
+// https://youtu.be/ed8SzALpx1Q?t=1481
