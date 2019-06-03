b6da8a044be0e191990ed2df34e124a02649612f
Fabrizio <Fabrizio_39@hotmail.com>
2019-06-03 19:40:05 +0200

    Cleaned up folder directory and replaced /server

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
-const books = require('../data/datastore');
+const { books, authors } = require('../data/datastore');
 // Destructioring function
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 // Schema describes the data on this kind of graph:
 // - Define object types
@@ -12,9 +12,19 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    }),
+});
+
+// Book object type on the graph
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     }),
 });
 
@@ -24,7 +34,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // code to get data from db / othe source
                 return books.find(book => book.id == args.id);
---
+++ server/spec/datastore.spec.js
@@ -0,0 +1,29 @@
+const books = require('../data/datastore');
+
+describe('The datastore', () => {
+
+    it('should contain three book objects.', () => {
+        // Assert
+        expect(books.length).toBe(3);
+    });
+
+    it('should return the correct requested object.', () => {
+        // Arrange
+        const testBook = { name: 'Name of the Wind', genre: 'Fantasy', id: '1' };
+        // Act
+        const actual = books.find(book => book.id == testBook.id);
+        // Assert
+        expect(actual).toBeTruthy;
+        expect(actual).toEqual(testBook);
+    });
+
+    it('should contain the correct expected content.', () => {
+        // Arrange
+        const testBook = { name: 'Name of the Wind', genre: 'Fantasy', id: '1' };
+        // Act
+        const actual = books.find(book => book.id == 1);
+        // Assert
+        expect(actual.name).toContain('the Wind', 'Name of');
+        expect(actual.genre).toBe(testBook.genre);
+    });
+});
