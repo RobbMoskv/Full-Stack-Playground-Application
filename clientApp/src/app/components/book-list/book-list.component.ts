f886159774678ded813914eb94ec9aac322fd56e
Fabrizio <Fabrizio_39@hotmail.com>
2019-07-15 12:44:19 +0200

    Implemente frist graphql query

--- clientApp/src/app/components/book-list/book-list.component.ts
+++ clientApp/src/app/components/book-list/book-list.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subscription } from 'rxjs';
+
+// We use the gql tag to parse our query string into a query document
+const AllBooks = gql`
+  query {
+    authors {
+      name
+    }
+  }
+`;
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
+  loading: boolean;
+  authors: any;
 
-  constructor() { }
+  private querySubscription: Subscription;
+
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: AllBooks
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.authors = data.authors;
+      });
   }
 
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
 }
