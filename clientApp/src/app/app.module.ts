4da3a30e0534f2b083cb872caf87e303026e4fb7
Fabrizio <Fabrizio_39@hotmail.com>
2019-07-15 12:44:42 +0200

    Import apollo/graphql related modules

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+// Apollo client specific import
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Components
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [AppComponent, BookListComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          // connect client to an external GraphQL Server
+          link: httpLink.create({
+            uri: 'http://localhost:4000/graphql'
+          })
+        };
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
