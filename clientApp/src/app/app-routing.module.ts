8a0550533f795f3185092887fa7d1d98d3409fca
Fabrizio <Fabrizio_39@hotmail.com>
2019-07-15 16:27:14 +0200

    Adapt routing to include author list

--- clientApp/src/app/app-routing.module.ts
+++ clientApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
 
-const routes: Routes = [{ path: 'books', component: BookListComponent }];
+const routes: Routes = [
+  { path: 'books', component: BookListComponent },
+  { path: 'authors', component: AuthorListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
