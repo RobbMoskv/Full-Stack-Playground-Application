f0356ebcf87aef6ecb9e52645b528706e9963ab2
Fabrizio <Fabrizio_39@hotmail.com>
2019-07-28 20:04:35 +0200

    dummy data for initial setup

---
+++ graphql/dbcontext/data/datastore.js
@@ -0,0 +1,25 @@
+//Dummy data for lazy ones :) 
+const books = [
+    { name: 'Name of the Wind', genre: 'Romantic' },
+    { name: 'The Final Empire', genre: 'Fantasy' },
+    { name: 'The Long Earth', genre: 'Sci-Fi' },
+    { name: 'The Hero of Ages', genre: 'Fantasy' },
+    { name: 'The Colour of Magic', genre: 'Sci-Fi' },
+    { name: 'The Light Fantastic', genre: 'Fantasy' },
+    { name: 'Long days pass by', genre: 'Crime' },
+    { name: 'Never ending story', genre: 'Fantasy' },
+    { name: 'Last man on earth', genre: 'Biograhpy' },
+    { name: 'The great war', genre: 'Politics' },
+];
+
+const authors = [
+    { name: 'Patrick Rothfuss', age: 44 },
+    { name: 'Brandon Sanderson', age: 42 },
+    { name: 'Marc Terency', age: 53 },
+    { name: 'Alica Key', age: 34 },
+    { name: 'Alexandra Partton', age: 64 },
+    { name: 'Michael Kellis', age: 23 },
+    { name: 'Carla Johnson', age: 44 },
+];
+
+module.exports = { books, authors };
