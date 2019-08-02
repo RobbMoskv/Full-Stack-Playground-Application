65f7e8d159b43b79972a6ed3f41260caf67eab06
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-02 23:31:14 +0200

    Extend datastore

--- graphql/dbcontext/data/datastore.js
+++ graphql/dbcontext/data/datastore.js
@@ -1,25 +1,29 @@
 //Dummy data for lazy ones :) 
 const books = [
-    { name: 'Name of the Wind', genre: 'Romantic' },
-    { name: 'The Final Empire', genre: 'Fantasy' },
-    { name: 'The Long Earth', genre: 'Sci-Fi' },
-    { name: 'The Hero of Ages', genre: 'Fantasy' },
-    { name: 'The Colour of Magic', genre: 'Sci-Fi' },
-    { name: 'The Light Fantastic', genre: 'Fantasy' },
-    { name: 'Long days pass by', genre: 'Crime' },
-    { name: 'Never ending story', genre: 'Fantasy' },
-    { name: 'Last man on earth', genre: 'Biograhpy' },
-    { name: 'The great war', genre: 'Politics' },
+    { name: 'Name of the Wind', genre: 'Romantic', authorid: '' },
+    { name: 'The Final Empire', genre: 'Fantasy', authorid: '' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', authorid: '' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', authorid: '' },
+    { name: 'The Colour of Magic', genre: 'Sci-Fi', authorid: '' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', authorid: '' },
+    { name: 'Long days pass by', genre: 'Crime', authorid: '' },
+    { name: 'Never ending story', genre: 'Fantasy', authorid: '' },
+    { name: 'Last man on earth', genre: 'Biograhpy', authorid: '' },
+    { name: 'The great war', genre: 'Politics', authorid: '' },
+    { name: 'The big wall', genre: 'Biography', authorid: '' },
+    { name: 'Wedding crasher', genre: 'Comedy', authorid: '' },
+    { name: 'The big planet', genre: 'Politics', authorid: '' },
+    { name: 'Me, myself and I', genre: 'Politics', authorid: '' },
 ];
 
 const authors = [
-    { name: 'Patrick Rothfuss', age: 44 },
-    { name: 'Brandon Sanderson', age: 42 },
-    { name: 'Marc Terency', age: 53 },
-    { name: 'Alica Key', age: 34 },
-    { name: 'Alexandra Partton', age: 64 },
-    { name: 'Michael Kellis', age: 23 },
-    { name: 'Carla Johnson', age: 44 },
+    { name: 'Patrick Rothfuss', age: 44, id: '' },
+    { name: 'Brandon Sanderson', age: 42, id: '' },
+    { name: 'Marc Terency', age: 53, id: '' },
+    { name: 'Alica Key', age: 34, id: '' },
+    { name: 'Alexandra Partton', age: 64, id: '' },
+    { name: 'Michael Kellis', age: 23, id: '' },
+    { name: 'Carla Johnson', age: 44, id: '' },
 ];
 
 module.exports = { books, authors };
