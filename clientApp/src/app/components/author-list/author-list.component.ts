28bee5110157e14a6bff3c3b9d9d8eacf97543a7
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-03 22:43:02 +0200

    Inject bookstore service and outsource logic into service

--- clientApp/src/app/components/author-list/author-list.component.ts
+++ clientApp/src/app/components/author-list/author-list.component.ts
@@ -1,44 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
-import { Subscription } from 'rxjs';
-
-// We use the gql tag to parse our query string into a query document
-const AllAuthors = gql`
-  query {
-    authors {
-      name
-      age
-    }
-  }
-`;
+import { Component, OnInit } from '@angular/core';
+import { BookStoreService } from 'src/app/services/book-store.service';
 
 @Component({
   selector: 'app-author-list',
   templateUrl: './author-list.component.html',
   styleUrls: ['./author-list.component.css']
 })
-export class AuthorListComponent implements OnInit, OnDestroy {
-  loading: boolean;
+export class AuthorListComponent implements OnInit {
   authors: any[];
 
-  private querySubscription: Subscription;
-
-  constructor(private apollo: Apollo) {}
+  constructor(private bs: BookStoreService) {}
 
   ngOnInit() {
-    this.querySubscription = this.apollo
-      .watchQuery<any>({
-        query: AllAuthors
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        // tslint:disable-next-line:no-debugger
-        this.loading = loading;
-        this.authors = data.authors;
-      });
-  }
-
-  ngOnDestroy() {
-    this.querySubscription.unsubscribe();
+    this.authors = this.bs.getAllAuthors();
   }
 }
